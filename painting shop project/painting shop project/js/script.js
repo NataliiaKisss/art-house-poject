2e039b43a17f7c77d172e45907c21a3061b1a2eb
NataliiaKisss <nataliia.kisilishyna@gmail.com>
2025-12-27 22:12:36 +0200

    Delete painting shop project/painting shop project directory

--- painting shop project/painting shop project/js/script.js
+++
@@ -1,6 +0,0 @@
-const openBtn = document.querySelector('.burger__btn');
-const nav = document.querySelector('.header__nav');
-
-openBtn.addEventListener('click', () => {
-  nav.classList.toggle('header__nav--active');
-});
